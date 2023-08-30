5f65b8beff9aad8e65bc721b3399d8a7f08d4e47
Reyaz <Reyaz@dxminds.com>
2023-08-31 00:10:28 +0530

    firstStateChange

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [someState,setSomeState] = useState("")
   return (
     <div>
       <h1>Movies Search App</h1>
+      <button onClick={()=>setSomeState("newState")}>setState</button>
+
+      <h4>{someState}</h4>
     </div>
   )
 }
