b0526745f29cad7406eea310cf732d8ec02654c1
Reyaz <riyaz.shaik.1481@gmail.com>
2023-09-02 08:22:31 +0530

    improvising

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,19 +1,36 @@
 import React, { useState } from 'react';
 import './navbar.css';
 import { Link } from 'react-router-dom';
+import { TextField } from '@mui/material';
 
 function Navbar() {
     const [searchStarted,setSearchStarted] = useState(false);
+    const [searchName,setSearchName] = useState("");
+
+    const handleSearchStart = () => {
+        setSearchStarted(true)
+    }
+
+    const handleSearchOnChange = (e) => {
+        setSearchName(e.target.value)
+    }
   return (
-    <div className='navbar_head'>
-      <h1 className='navbar_title'>Movies Searching</h1>
-      <div className='navbar_menu'>
-        <Link to='/'>Home</Link>
-        <a href='#'>Search</a>
-        <Link to='/login'>SignUp/Login</Link>
-        <Link to='/favorites'>Favorites</Link>
-      </div>
-    </div>
+    <>{
+        searchStarted
+        ?
+        <TextField value={searchName} onChange={handleSearchOnChange} placeholder="Search..." />
+        :
+        <div className='navbar_head'>
+            <h1 className='navbar_title'>Movies Searching</h1>
+            <div className='navbar_menu'>
+                <Link to='/'>Home</Link>
+                <a onClick={handleSearchStart} href='#'>Search</a>
+                <Link to='/login'>SignUp/Login</Link>
+                <Link to='/favorites'>Favorites</Link>
+            </div>
+        </div>
+    }</>
+    
   )
 }
 
