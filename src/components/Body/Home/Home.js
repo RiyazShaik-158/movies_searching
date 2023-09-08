c5f8bec753b3b3076417baaae9424b3396c44da8
Reyaz <riyaz.shaik.1481@gmail.com>
2023-09-09 00:17:21 +0530

    startingHomePageWithFakeData

--- src/components/Body/Home/Home.js
+++ src/components/Body/Home/Home.js
@@ -1,10 +1,29 @@
 import React from 'react';
 import './home.scss';
+const data = [
+  {
+  name : 'one',
+  },
+  {
+    name : 'two',
+  },
+  {
+    name : 'three'
+  }
+]
 
 function Home() {
   return (
     <div className='homeMain'>
-      
+      {
+        data.map((item,index) => {
+          return(
+            <div key={index}>
+              <h4>{item.name}</h4>
+            </div>
+          )
+        })
+      }
     </div>
   )
 }
