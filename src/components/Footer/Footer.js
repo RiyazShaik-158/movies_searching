4d85733399e7f499486252e01b893d2177b8c2f2
Reyaz <riyaz.shaik.1481@gmail.com>
2023-09-10 09:47:12 +0530

    updatingFooterBasicDesign

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,7 +5,28 @@ function Footer() {
   return (
     <div className='Footer_main'>
       <footer className='Footer_content'>
-        <h1>Footer appears here</h1>
+        <div className='Footer_content_aboutDiv'>
+          <div className='Footer_aboutDiv'>
+            <p>Terms of service</p>
+            <p>Movies</p>
+            <p>Contact</p>
+            <p>TV series</p>
+          </div>
+          <div className='Genre'>
+            <p>Action</p>
+            <p>Fantasy</p>
+            <p>Comedy</p>
+            <p>Horror</p>
+            <p>Drama</p>
+            <p>Mystery</p>
+          </div>
+          <div className='Connect us'>
+            <div><p>Join Group Telegram</p></div>
+            <div><p>Join Group Reddit</p></div>
+            <div><p>Join Twitter</p></div>
+          </div>
+        </div>
+        <div className='goku_logo_again'></div>
       </footer>
     </div>
   )
