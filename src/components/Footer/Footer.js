2cc4a6a996a74eb352e352a8fe5edeae3eee68e8
Reyaz <riyaz.shaik.1481@gmail.com>
2023-09-11 22:32:47 +0530

    footerDesign3Quarter

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,19 +6,19 @@ function Footer() {
     <div className='Footer_main'>
       <footer className='Footer_content'>
         <div className='Footer_content_aboutDiv'>
-          <div className='Footer_aboutDiv'>
+          <div className='Footer_aboutDiv_bigger_for_forAbout'>
             <h4>About Us</h4>
-            <div className='subMenu_inFooter'>
+            <div className='subMenu_inFooter1'>
               <p>Terms of service</p>
               <p>Movies</p>
               <p>Contact</p>
-              <p>TV series</p>
+              <p>TV Series</p>
             </div>
             
           </div>
           <div className='Footer_aboutDiv'>
             <h4>Genre</h4>
-            <div className='subMenu_inFooter'>
+            <div className='subMenu_inFooter_genre'>
               <p>Action</p>
               <p>Fantasy</p>
               <p>Comedy</p>
@@ -30,7 +30,7 @@ function Footer() {
           </div>
           <div className='Footer_aboutDiv'>
             <h4>Connect us</h4>
-            <div className='subMenu_inFooter'>
+            <div className='subMenu_inFooter_connections'>
               <div><p>Join Group Telegram</p></div>
               <div><p>Join Group Reddit</p></div>
               <div><p>Join Twitter</p></div>
@@ -42,6 +42,7 @@ function Footer() {
           <img src='gokuLogo.png' alt='gokuLogo'/>
         </div>
       </footer>
+      <div className='endingCopy'><span>&copy; 2023 GOKU with 🧡</span></div>
     </div>
   )
 }
