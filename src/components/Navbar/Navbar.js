544ac616975955d1a6eb1fe7ad85322beb83d2b9
Reyaz <riyaz.shaik.1481@gmail.com>
2023-09-14 09:15:54 +0530

    makingGenreInNav

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -5,10 +5,16 @@ import HighlightOffIcon from '@mui/icons-material/HighlightOff';
 import { Link } from 'react-router-dom';
 import SearchIcon from '@mui/icons-material/Search';
 import PersonIcon from '@mui/icons-material/Person';
+import { genres } from './forGenres';
+import { Popover } from '@mui/material';
 
 function Navbar({searchText,setSearchText}) {
 
   const [searchStarting,setSearchStarting] = useState(false);
+
+  // for popover
+  const [anchorEl, setAnchorEl] = useState(null);
+
 
   const handleSearchClose = () => {
     setSearchStarting(false);
@@ -17,6 +23,21 @@ function Navbar({searchText,setSearchText}) {
   useEffect(()=>{
 
   },[searchStarting])
+
+
+  // for popover
+  const handlePopoverOpen = (event) => {
+    console.log(event.currentTarget)
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handlePopoverClose = () => {
+    setAnchorEl(null);
+  };
+
+  const open = Boolean(anchorEl);
+
+
   return (
     <>
       {
@@ -40,7 +61,58 @@ function Navbar({searchText,setSearchText}) {
           <div className='App_menu'>
             <div className='menu1'>
                 <Link to='/home'>Home</Link>
-                <div className='GenreDiv'><h4>Genre</h4><KeyboardArrowDownIcon className="downwardIcon"/></div>
+                {/* for popover , need to write below that element only */}
+                <div className='GenreDiv'
+                  aria-owns={open ? 'mouse-over-popover' : undefined}
+                  aria-haspopup="true"
+                  onMouseEnter={handlePopoverOpen}
+                  onMouseLeave={handlePopoverClose}
+                >
+                  <h4>Genre</h4><KeyboardArrowDownIcon className="downwardIcon"/>
+                </div>
+                <Popover
+                  id="mouse-over-popover"
+                  sx={{
+                    pointerEvents: 'none'
+                  }}
+                  open={open}
+                  anchorEl={anchorEl}                
+                  anchorOrigin={{
+                    vertical: 'top',
+                    horizontal: 'left',
+                  }}
+                  transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'left',
+                  }}
+                  className='forPopover_popping'
+                  onClose={handlePopoverClose}
+                  // disableRestoreFocus
+                >
+                  {/* <Typography sx={{ p: 1 }}>I use Popover.</Typography> */}
+                  <div 
+                    style={{
+                      width:'400px',
+                      height:'370px',
+                      backgroundColor:'#000',
+                      color:'#fff',
+                      display:'flex',
+                      flexWrap:'wrap',
+                      columnGap:'20px',
+                      padding:'40px'
+                    }} 
+                    className='genre_popover_div'
+                  >
+                    {
+                      genres.map((item,idx)=> {
+                        return(
+                          <p key={idx} style={{fontSize:'14px',width:'30%',pointerEvents:'all'}}>{item}</p>
+                        )
+                      })
+                    }
+                    {/* <h4>testing popover</h4> */}
+                  </div>
+                </Popover>
                 <div className='GenreDiv'><h4>Country</h4><KeyboardArrowDownIcon className="downwardIcon"/></div>
                 <Link to="/movies">Movies</Link>
                 <Link to="/tvseries">TV Series</Link>
---
+++ src/components/Navbar/forGenres.js
@@ -0,0 +1,12 @@
+export const genres = [
+    "Drama","Comedy","Thriller",
+    "Action","Romance","Horror",
+    "Crime","Documentary","Adventure",
+    "Mystery","Fantasy","Family",
+    "Science Fiction","TV Movie","Animation",
+    "History","Music","War",
+    "Western","Sci-Fi & Fantasy","Action & Adve...",
+    "Reality","War & Politics","Kids",
+    "Talk","Soap","News",
+    "Musical"
+]
