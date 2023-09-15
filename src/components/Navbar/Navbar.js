7ee931d05e7ea6e0a5fa4e713312101980ecf49d
Reyaz <riyaz.shaik.1481@gmail.com>
2023-09-15 07:48:29 +0530

    updating movies hover

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import './navbar.scss';
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 import HighlightOffIcon from '@mui/icons-material/HighlightOff';
@@ -6,132 +6,136 @@ import { Link } from 'react-router-dom';
 import SearchIcon from '@mui/icons-material/Search';
 import PersonIcon from '@mui/icons-material/Person';
 import { genres } from './forGenres';
+import { countries } from './forCountries';
 import { Popover } from '@mui/material';
 
-function Navbar({searchText,setSearchText}) {
-
-  const [searchStarting,setSearchStarting] = useState(false);
-
-  // for popover
+function Navbar({ searchText, setSearchText }) {
+  const [searchStarting, setSearchStarting] = useState(false);
   const [anchorEl, setAnchorEl] = useState(null);
-
+  const [popoverContent, setPopoverContent] = useState(null);
 
   const handleSearchClose = () => {
     setSearchStarting(false);
   }
 
-  useEffect(()=>{
-
-  },[searchStarting])
-
-
-  // for popover
-  const handlePopoverOpen = (event) => {
-    console.log(event.currentTarget)
+  const handleMouseEnter = (event, content) => {
     setAnchorEl(event.currentTarget);
+    console.log(content)
+    setPopoverContent(content);
   };
 
   const handlePopoverClose = () => {
     setAnchorEl(null);
+    setPopoverContent(null);
   };
 
   const open = Boolean(anchorEl);
 
-
   return (
     <>
-      {
-        searchStarting 
-        ?
-        // <h1 onClick={setSearchStarting(false)}>Search box will come here</h1>
+      {searchStarting ? (
         <div className='Navbar_head'>
           <div className='searchNewDiv'>
             <div className='forSearch'>
               <SearchIcon />
-              <input autoFocus className='Navbar_searchInput' placeholder='Enter keywords...' value={searchText} onChange={(e) => setSearchText(e.target.value)}/>
+              <input
+                autoFocus
+                className='Navbar_searchInput'
+                placeholder='Enter keywords...'
+                value={searchText}
+                onChange={(e) => setSearchText(e.target.value)}
+              />
             </div>
-            <HighlightOffIcon onClick={handleSearchClose} className='closeButton_search'/>
+            <HighlightOffIcon onClick={handleSearchClose} className='closeButton_search' />
           </div>
         </div>
-        :
+      ) : (
         <div className='Navbar_head'>
           <div className='App_logoContainer'>
-            <Link to='/'><img src="gokuLogo.png" alt="GOKU Logo" title="GOKU" className='App_logo' /></Link>
+            <Link to='/'>
+              <img src="gokuLogo.png" alt="GOKU Logo" title="GOKU" className='App_logo' />
+            </Link>
           </div>
           <div className='App_menu'>
             <div className='menu1'>
-                <Link to='/home'>Home</Link>
-                {/* for popover , need to write below that element only */}
-                <div className='GenreDiv'
-                  aria-owns={open ? 'mouse-over-popover' : undefined}
-                  aria-haspopup="true"
-                  onMouseEnter={handlePopoverOpen}
-                  onMouseLeave={handlePopoverClose}
-                >
-                  <h4>Genre</h4><KeyboardArrowDownIcon className="downwardIcon"/>
-                </div>
-                <Popover
-                  id="mouse-over-popover"
-                  sx={{
-                    pointerEvents: 'none'
-                  }}
-                  open={open}
-                  anchorEl={anchorEl}                
-                  anchorOrigin={{
-                    vertical: 'top',
-                    horizontal: 'left',
-                  }}
-                  transformOrigin={{
-                    vertical: 'top',
-                    horizontal: 'left',
-                  }}
-                  className='forPopover_popping'
-                  onClose={handlePopoverClose}
-                  // disableRestoreFocus
-                >
-                  {/* <Typography sx={{ p: 1 }}>I use Popover.</Typography> */}
-                  <div 
+              <Link to='/home'>Home</Link>
+              <div
+                className='GenreDiv'
+                aria-owns={open ? 'mouse-over-popover' : undefined}
+                aria-haspopup="true"
+                onMouseEnter={(event) => handleMouseEnter(event, genres)}
+                // onMouseLeave={handlePopoverClose}
+              >
+                <h4>Genre</h4>
+                <KeyboardArrowDownIcon className="downwardIcon" />
+              </div>
+              <div
+                className='GenreDiv'
+                aria-owns={open ? 'mouse-over-popover' : undefined}
+                aria-haspopup="true"
+                onMouseEnter={(event) => handleMouseEnter(event, countries)}
+                // onMouseLeave={handlePopoverClose}
+              >
+                <h4>Country</h4>
+                <KeyboardArrowDownIcon className="downwardIcon" />
+              </div>
+              <Popover
+                id="mouse-over-popover"
+                sx={{
+                  pointerEvents: 'none'
+                }}
+                open={open}
+                anchorEl={anchorEl}
+                anchorOrigin={{
+                  vertical: 'top',
+                  horizontal: 'left',
+                }}
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'left',
+                }}
+                className='forPopover_popping'
+                onClose={handlePopoverClose}
+              >
+                {popoverContent && (
+                  <div
                     style={{
-                      width:'400px',
-                      height:'370px',
-                      backgroundColor:'#000',
-                      color:'#fff',
-                      display:'flex',
-                      flexWrap:'wrap',
-                      columnGap:'20px',
-                      padding:'40px'
-                    }} 
+                      width: '400px',
+                      height: '370px',
+                      backgroundColor: '#000',
+                      color: '#fff',
+                      display: 'flex',
+                      flexWrap: 'wrap',
+                      columnGap: '20px',
+                      padding: '40px'
+                    }}
                     className='genre_popover_div'
                   >
-                    {
-                      genres.map((item,idx)=> {
-                        return(
-                          <p key={idx} style={{fontSize:'14px',width:'30%',pointerEvents:'all'}}>{item}</p>
-                        )
-                      })
-                    }
-                    {/* <h4>testing popover</h4> */}
+                    {popoverContent.map((item, idx) => (
+                      <p onClick={handlePopoverClose} key={idx} style={{ fontSize: '14px', width: '30%', pointerEvents: 'all','&:hover':{cursor:'pointer',color:'#e64d25'} }}>{item}</p>
+                    ))}
                   </div>
-                </Popover>
-                <div className='GenreDiv'><h4>Country</h4><KeyboardArrowDownIcon className="downwardIcon"/></div>
-                <Link to="/movies">Movies</Link>
-                <Link to="/tvseries">TV Series</Link>
+                )}
+              </Popover>
+              <Link to="/movies">Movies</Link>
+              <Link to="/tvseries">TV Series</Link>
             </div>
             <div className='menu2'>
-                {/* <Link onClick={()=>setSearchStarting(true)}> */}
-                  <div className='GenreDiv' onClick={()=>setSearchStarting(true)}><SearchIcon fontSize='small' className="downwardIcon"/><h4>Search</h4></div>
-                {/* </Link> */}
-                
-                
-                <Link to="/login">
-                  <div className='GenreDiv'><PersonIcon className="downwardIcon"/><h4>Login</h4></div>
-                </Link>
+              <div className='GenreDiv' onClick={() => setSearchStarting(true)}>
+                <SearchIcon fontSize='small' className="downwardIcon" />
+                <h4>Search</h4>
               </div>
+              <Link to="/login">
+                <div className='GenreDiv'>
+                  <PersonIcon className="downwardIcon" />
+                  <h4>Login</h4>
+                </div>
+              </Link>
             </div>
+          </div>
         </div>
-      }
+      )}
     </>
-    
   )
 }
 
---
+++ src/components/Navbar/forCountries.js
@@ -0,0 +1,13 @@
+export const countries = [
+    "United States ... "," United Kingdom ", "France",
+    "Canada" , "Germany" , "Japan" ,
+    "Italy", "India" , "Spain" ,
+    "Australia" , "Hong Kong" , "South Korea" , 
+    "China" , "Belgium" , "Sweden" , 
+    "Mexico" , "Denmark" , "Ireland" , 
+    "Poland" , "Russia" , "Netherlands", 
+    "Brazil" , "Norway" , "Argentina" , 
+    "South Africa" , "Finland" , "Switzerland",
+    "New Zealand" , "Austria" , "Thailand" , 
+    "Turkey" , "Hungary" , "Czech Republic"
+]
