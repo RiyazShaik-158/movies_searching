0bee15bd15b98cb301e96013f32074fd07ec9268
Reyaz <riyaz.shaik.1481@gmail.com>
2023-09-15 08:31:52 +0530

    needToHideFooterFromLogin

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import './App.scss'
 import Navbar from './components/Navbar/Navbar';
-import { BrowserRouter, Route, Routes  } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, useNavigate  } from 'react-router-dom';
 import HomeWelcome from './components/Body/HomeWelcome/HomeWelcome';
 import Home from './components/Body/Home/Home';
 import Login from './components/Body/Login/Login';
@@ -9,7 +9,7 @@ import Footer from './components/Footer/Footer';
 
 function App() {
   const [searchText,setSearchText] = useState("");
-
+  const navigate = useNavigate()
 
   return (
     <div className='App'>
@@ -20,6 +20,8 @@ function App() {
             <Route path='/home' element={<Home/>}></Route>
             <Route path='/login' element={<Login/>}></Route>
           </Routes>
+
+        {!navigate().location.pathname === '/login' && <Footer/>}
 
         <Footer/>
       </BrowserRouter>
