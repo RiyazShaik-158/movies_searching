094f2ac93268e225b3d977184b37bcbeec091e26
Reyaz <riyaz.shaik.1481@gmail.com>
2023-09-17 14:26:04 +0530

    trying sample fetch result

--- src/components/Navbar/onChangeSearchResults/SearchResults.js
+++ src/components/Navbar/onChangeSearchResults/SearchResults.js
@@ -1,10 +1,18 @@
 import React from 'react';
 
-function SearchResults({fakeSearch}) {
+function SearchResults({moviesData}) {
   return (
-    <div>
-      <h4>on Change search based movies appear here</h4>
-      <h1>Searched Movie name : {fakeSearch}</h1>
+    <div style={{marginTop:'40px',color:'#fff',display:'flex',flexWrap:'wrap',columnGap:'40px',rowGap:'20px'}}>
+      {
+        moviesData?.map(item => {
+          return(
+            <div style={{display:'flex',flexDirection:'column',width:'250px'}}>
+              <h4>{item.Title}</h4>
+              <img style={{aspectRatio:'4:3',objectFit:'contain'}} width={'200px'} src={item.Poster} alt='someImg'/>
+            </div>
+          )
+        })
+      }
     </div>
   )
 }
