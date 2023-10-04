dce79888d4994708488d9833c75a65bce234782b
Reyaz <riyaz.shaik.1481@gmail.com>
2023-10-04 11:05:33 +0530

    trying sample lodash

--- src/App.js
+++ src/App.js
@@ -1,32 +1,45 @@
-import React, { useState } from 'react';
-import './App.scss'
-import Navbar from './components/Navbar/Navbar';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import HomeWelcome from './components/Body/HomeWelcome/HomeWelcome';
-import Home from './components/Body/Home/Home';
-import Login from './components/Body/Login/Login';
-// import ForgotPassword from './components/Body/Login/LoginKids/ForgotPassword';
-// import Footer from './components/Footer/Footer';
+// import React, { useState } from 'react';
+// import './App.scss'
+// import Navbar from './components/Navbar/Navbar';
+// import { BrowserRouter, Route, Routes } from 'react-router-dom';
+// import HomeWelcome from './components/Body/HomeWelcome/HomeWelcome';
+// import Home from './components/Body/Home/Home';
+// import Login from './components/Body/Login/Login';
+// // import ForgotPassword from './components/Body/Login/LoginKids/ForgotPassword';
+// // import Footer from './components/Footer/Footer';
+
+// function App() {
+//   const [searchText,setSearchText] = useState("");
+//   const [userLoggedIn,setUserLoggedIn] = useState(false);
+
+//   return (
+//     <div className='App'>
+//       <BrowserRouter>
+//         <Navbar setSearchText={setSearchText} searchText={searchText}/>
+//           <Routes>
+//             <Route path='/' element={<HomeWelcome/>}></Route>
+//             <Route path='/home' element={<Home/>}></Route>
+//             <Route path='/login' element={<Login userLoggedIn={userLoggedIn} setUserLoggedIn={setUserLoggedIn}  />}></Route>
+//             {/* <Route path='/login/' element={<ForgotPassword />}></Route> */}
+//           </Routes>
+
+//         {/* <Footer/> */}
+//       </BrowserRouter>
+//     </div>
+//   )
+// }
+
+// export default App
+
+import React from 'react'
+import Sample from './components/Body/sample'
 
 function App() {
-  const [searchText,setSearchText] = useState("");
-  const [userLoggedIn,setUserLoggedIn] = useState(false);
-
   return (
-    <div className='App'>
-      <BrowserRouter>
-        <Navbar setSearchText={setSearchText} searchText={searchText}/>
-          <Routes>
-            <Route path='/' element={<HomeWelcome/>}></Route>
-            <Route path='/home' element={<Home/>}></Route>
-            <Route path='/login' element={<Login userLoggedIn={userLoggedIn} setUserLoggedIn={setUserLoggedIn}  />}></Route>
-            {/* <Route path='/login/' element={<ForgotPassword />}></Route> */}
-          </Routes>
-
-        {/* <Footer/> */}
-      </BrowserRouter>
+    <div>
+      <Sample />
     </div>
   )
 }
 
-export default App
+export default App;
---
+++ src/components/Body/sample.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import _, { size } from 'lodash';
+
+function Sample() {
+    console.log(_.join([1,2,3,4,100,0],[separator='-']));
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Sample
